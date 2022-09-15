518af6c93dd39289a73bdfe9e7bf646abe28e5f3
martijnkunstman <martijn@kunstman.nl>
2022-09-15 14:27:07 +0200

    refactor

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <?php
 //use composer packages
 require __DIR__ . '/vendor/autoload.php';
-
 //load jsonData
-$rawJSONData = file_get_contents('data.json');//https://annexbios.nickvz.nl/api/
+$rawJSONData = file_get_contents('data.json'); //data.json from https://annexbios.nickvz.nl/api/
 $jsonData = json_decode($rawJSONData, true);
-
 //instance mustache
 $mustache = new Mustache_Engine(array('entity_flags' => ENT_QUOTES, 'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates')));
 //load mustache template
 $movieBlockTemplate = $mustache->loadTemplate('movieBlock');
 $htmlTemplate = $mustache->loadTemplate('html');
-?>
-
-  <?php
-  $htmlData["body"] = "";
-  for ($i = 0; $i < count($jsonData["filmdata"]); $i++) {
-    //render mustache template and insert data from jsonData
-    $htmlData["body"] = $htmlData["body"].$movieBlockTemplate->render($jsonData["filmdata"][$i]);
-  } 
-  echo $htmlTemplate->render($htmlData);
-
-
-  ?>
+//create $htmlData
+$htmlData["body"] = "";
+//loop $jsonData["filmdata"] to get all movies
+for ($i = 0; $i < count($jsonData["filmdata"]); $i++) {
+  //add render data to $htmlData using $movieBlockTemplate and $jsonData["filmdata"]
+  $htmlData["body"] = $htmlData["body"] . $movieBlockTemplate->render($jsonData["filmdata"][$i]);
+}
+//render $htmlData in $htmlTemplate
+echo $htmlTemplate->render($htmlData);
